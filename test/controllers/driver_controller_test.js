24a3a1884ebac5ffc57fee2d7c8fd32e1beba5b3
Priyanka Negi <prinegi100@gmail.com>
2018-04-08 11:35:32 +0530

    -test POST /api/drivers

--- test/controllers/driver_controller_test.js
+++ test/controllers/driver_controller_test.js
@@ -4,10 +4,10 @@ const app = require('../../app');
 describe('Drivers controller', () => {
   it('Post to /api/drivers creates a new driver', done => {
     request(app)
-    .post('/api/drivers')
-    .send({email: 'test@test.com'})
-    .end(() => {
-      done();
-    })
-  })
-})
+      .post('/api/drivers')
+      .send({ email: 'test@test.com' })
+      .end(() => {
+        done();
+      });
+  });
+});
