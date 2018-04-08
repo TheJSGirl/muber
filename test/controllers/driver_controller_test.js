8b596481fdaf34f714693ddcd5f40d898965608b
Priyanka Negi <prinegi100@gmail.com>
2018-04-08 11:24:04 +0530

    -created test for  POST /api/drivers

---
+++ test/controllers/driver_controller_test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Drivers controller', () => {
+  it('Post to /api/drivers creates a new driver', done => {
+    request(app)
+    .post('/api/drivers')
+    .send({email: 'test@test.com'})
+    .end(() => {
+      done();
+    })
+  })
+})
