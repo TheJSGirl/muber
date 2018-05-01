218298fd0980839594931172d1a6a1be0c02cac5
Priyanka Negi <prinegi100@gmail.com>
2018-05-02 01:06:36 +0530

    created driver_controller & driver_controller_test

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/27017/muber');
 
 routes(app);
 module.exports = app;
--- controllers/driver-controller.js
+++ controllers/driver-controller.js
@@ -1,8 +1,11 @@
+const Driver = require('../models/driver');
 module.exports = {
   greeting(req, res) {
     res.send({ hi: 'there' });
   },
   create(req, res) {
-    res.send({ hi: 'there' });
+    // res.send({ hi: 'there' });
+    const driverProps = req.body;
+    Driver.create(driverProps).then(driver => res.send(driver));
   }
 };
